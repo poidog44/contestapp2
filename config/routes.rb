60ffcd1e4c32412d87cc9252526d0f1fdc1ed494
Stuart Kam <stuart.kam17@gmail.com>
2016-08-14 18:40:35 -1000

    account model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Contestapp2::Application.routes.draw do
+  resources :accounts
+
   root "dashboard#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
